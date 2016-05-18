9916278d211ebfcfa4de6a29cc801000e3b5b40d
Paulo Santos <psantos@blackducksoftware.com>
2016-05-18 13:43:50 -0400

    Initial commit of risk report tab creation per build

---
+++ src/main/java/com/blackducksoftware/integration/hub/bamboo/reports/HubRiskReportAction.java
@@ -0,0 +1,13 @@
+package com.blackducksoftware.integration.hub.bamboo.reports;
+
+import com.atlassian.bamboo.ww2.BambooActionSupport;
+
+public class HubRiskReportAction extends BambooActionSupport {
+
+	private static final long serialVersionUID = 4076165272346339757L;
+
+	@Override
+	public String doExecute() throws Exception {
+		return SUCCESS;
+	}
+}
