1b89b799d127beabf9d2645b16e898d8c4c8345a
jrichard <jrichard@blackducksoftware.com>
2016-10-13 14:41:36 -0400

    IBH-69 Should fix the report formatting issue

--- src/main/resources/js/HubReportStartup.js
+++ src/main/resources/js/HubReportStartup.js
@@ -25,20 +25,22 @@ if (pageBody) {
 	pageBody.className += " pageBody";
 }
 
-adjustWidth(document.getElementById("highVulnerabilityRiskBar"));
-adjustWidth(document.getElementById("mediumVulnerabilityRiskBar"));
-adjustWidth(document.getElementById("lowVulnerabilityRiskBar"));
-adjustWidth(document.getElementById("noVulnerabilityRiskBar"));
-
-adjustWidth(document.getElementById("highLicenseRiskBar"));
-adjustWidth(document.getElementById("mediumLicenseRiskBar"));
-adjustWidth(document.getElementById("lowLicenseRiskBar"));
-adjustWidth(document.getElementById("noLicenseRiskBar"));
-
-adjustWidth(document.getElementById("highOperationalRiskBar"));
-adjustWidth(document.getElementById("mediumOperationalRiskBar"));
-adjustWidth(document.getElementById("lowOperationalRiskBar"));
-adjustWidth(document.getElementById("noOperationalRiskBar"));
-
-adjustTable();
+function startup(){
+	adjustWidth(document.getElementById("highVulnerabilityRiskBar"));
+	adjustWidth(document.getElementById("mediumVulnerabilityRiskBar"));
+	adjustWidth(document.getElementById("lowVulnerabilityRiskBar"));
+	adjustWidth(document.getElementById("noVulnerabilityRiskBar"));
+	
+	adjustWidth(document.getElementById("highLicenseRiskBar"));
+	adjustWidth(document.getElementById("mediumLicenseRiskBar"));
+	adjustWidth(document.getElementById("lowLicenseRiskBar"));
+	adjustWidth(document.getElementById("noLicenseRiskBar"));
+	
+	adjustWidth(document.getElementById("highOperationalRiskBar"));
+	adjustWidth(document.getElementById("mediumOperationalRiskBar"));
+	adjustWidth(document.getElementById("lowOperationalRiskBar"));
+	adjustWidth(document.getElementById("noOperationalRiskBar"));
+	
+	adjustTable();
 sorttable.init();
+}
